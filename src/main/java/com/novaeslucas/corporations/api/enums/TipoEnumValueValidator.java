022713d21e235163b42675b979649d05c778d70a
Lucas Novaes <novaeslucas@gmail.com>
2023-03-05 13:11:31 -0300

    Adicionando Validator no Enum
    Para adicionar um validator em um enum, criar uma annotation Validator e implementar com o método de validacao. Essa configuracao não é aceita pelo swagger codegen, utilizamos constantes no yaml para reaproveitar patterns.

---
+++ src/main/java/com/novaeslucas/corporations/api/enums/TipoEnumValueValidator.java
@@ -0,0 +1,37 @@
+package com.novaeslucas.corporations.api.enums;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class TipoEnumValueValidator implements ConstraintValidator<TipoEnumValidator, String>
+{
+    private TipoEnumValidator annotation;
+
+    @Override
+    public void initialize(TipoEnumValidator annotation)
+    {
+        this.annotation = annotation;
+    }
+
+    @Override
+    public boolean isValid(String valueForValidation, ConstraintValidatorContext constraintValidatorContext)
+    {
+        boolean result = false;
+
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+
+        if(enumValues != null)
+        {
+            for(Object enumValue:enumValues)
+            {
+                if(valueForValidation.equals(enumValue.toString()))
+                {
+                    result = true;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
